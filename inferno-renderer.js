926ce91831ea544f602425657b239f6f6cb61d3e
viktor <viktor.movcan@bedegaming.com>
2018-08-13 09:47:32 +0100

    inferno component support

--- inferno-renderer.js
+++ inferno-renderer.js
@@ -1,6 +1,12 @@
-var inferno = require('inferno')
+var inferno = require('inferno');
+
+var isClass = (obj) => {
+  return (obj.prototype instanceof inferno.Component);
+};
 
 module.exports = function (template, data) {
-  var state = this.thisAsState ? this : data
-  inferno.render(template(state), this.el)
+  var state = this.thisAsState ? this : data;
+  var renderedTemplate = isClass(template) ? new template(state).render() : template(state);
+
+  inferno.render(renderedTemplate, this.el);
 }
